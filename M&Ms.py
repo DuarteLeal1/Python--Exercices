7149be8857fb3acb5f54b72e16a6810090282779
DuarteLeal1 <70014084+DuarteLeal1@users.noreply.github.com>
2020-12-26 10:35:29 +0000

    Update M&Ms.py

--- M&Ms.py
+++ M&Ms.py
@@ -1,20 +1,16 @@
 import random
 
-print("------------------------")
-print("    M&M guessing game!  ")
-print("------------------------")
-
-mm_count = random.randint(1, 100)
+random_number = random.randint(1, 100)
 attempt_limit = 5
 attempts = 0
 
 while attempts < attempt_limit:
-    guess_text = input("How manu M&Ms are in the jar? ")
+    guess_text = input("Whats your attempt? ")
     guess = int(guess_text)
     print(guess)
 
-    if mm_count == guess:
-        print(f"You got a free lunch! It was {guess}")
+    if random_number == guess:
+        print(f"Nice! It was {guess}")
         break
 
     elif guess < mm_count:
